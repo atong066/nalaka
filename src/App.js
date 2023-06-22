f0b1c4a610182c050258aa49121a0e2b4819d95d
atong066 <galiciavirgilio0@gmail.com>
2023-06-22 14:51:25 +0800

    test route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import './App.css';
-
+import React from "react";
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import Login from "./Components/Login/Login";
+import Register from "./Components/Register/Register";
+import NotFound from "./Components/RouteValidation/NoRoute";
 function App() {
   return (
-   <div>
-    hello world
-   </div>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Login/>}></Route>
+      <Route path="/Register" element={<Register/>}></Route>
+      <Route path="*" element={<NotFound/>} status={404} />
+    </Routes>
+  </BrowserRouter>
   );
 }
 
